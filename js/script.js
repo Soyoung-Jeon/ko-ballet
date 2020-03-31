2b18ef68e553d55f1e36227785fe1d1177dc6337
soyoung <jsoy0423@gmail.com>
2020-03-31 19:16:57 +0900

    스크립트 완료

--- js/script.js
+++ js/script.js
@@ -31,73 +31,37 @@ $(function(){
   })
 
 
+  // nav 이벤트
 
-  // 바로가기 이벤트
+  //초기화
+  $("nav a").eq(0).addClass("onText");
+  $("nav a").children().eq(0).addClass("onCircle");
 
-  // 바로가기 이벤트
-  // const sectionTop = $("section").offset().top;
+  // 클릭이벤트
+  $("nav a").click(function(e){
 
-  // $(".scrollBt").on({
-  //     click: function() {
-  //         $("html,body").stop().animate({ scrollTop: sectionTop }, 800);
-  //     }
-  // });
+    // 스크롤링
+    e.preventDefault();
+    $("html,body").animate({scrollTop:$(this.hash).offset().top}, 800);
 
-    // 네비게이션 이벤트
-      var contSumHeights = [0];
-      var heightSum = 0;
-      var $content = $(".content");
-      var contHegihts = [0]
+    var current = $("nav a").index(this);
+    
+    $("nav a").each(function(i){
+      // 클릭 시 CSS 변경
+      $(this).addClass("onText");
+      $(this).children().addClass("onCircle");
 
-      for (var i = 0; i < $content.length; i++) {
-  
-          var $el = $content.eq(i);
-          var height = $el.height() + parseInt($el.css("marginBottom").replace("px", ""));
-          
-          heightSum += height
-          contHegihts.push(height)
-          contSumHeights.push(heightSum);
+      if (current != i) {
+        $(this).removeClass("onText");
+        $(this).children().removeClass("onCircle");
       }
+    });
+
+  });
 
 
-      // 스크롤 시 네비게이션 효과
-      var currentIndex = 0
-
-      $(".contents-wrap").scroll(function() {
-          var current_scrollTop = parseInt($(this).scrollTop())
-          var selectedIndex = 0
-
-          contSumHeights.forEach((v, i)=>{
-              if(v < current_scrollTop + contHegihts[i]/2) selectedIndex = i
-              else return
-          })
-
-          if(selectedIndex != currentIndex){
-              currentIndex = selectedIndex    // 현재 인덱스 알려줌
-              $(".navBt").removeClass("navOn");
-              $(".navBt").css("color", "rgba(221, 221, 221, 0.5)");
-              $(".navBt").eq(selectedIndex).addClass("navOn");
-              $(".navBt").eq(selectedIndex).css("color", "#ddd");
-          }
-      }); 
-
-
-      // 네비게이션 버튼 클릭 효과
-      $(".navBt").click(function() {
-          var i = $(".navBt").index(this);
-          
-          var conTop = contSumHeights[i];
-
-          $(".contents-wrap").animate({ scrollTop: conTop }, 800);
-
-          $(".navBt").each(function(index) {
-              $(this).addClass("navOn");
-              $(this).css("color", "#ddd");
-
-              if (i != index) {
-                  $(this).removeClass("navOn");
-                  $(this).css("color", "rgba(221, 221, 221, 0.5)");
-              }
-          });
+  // smooth scroll 이벤트
+  
+      
 
 });
